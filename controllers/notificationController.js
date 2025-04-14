6dae4d19cf828e5338d26e235f66ec24815f8319
umairaltaf982 <umairaltaf982@gmail.com>
2025-04-14 11:14:02 +0500

    last commit

---
+++ controllers/notificationController.js
@@ -0,0 +1,105 @@
+const Student = require('../models/Student');
+const Subscription = require('../models/Subscription');
+
+exports.getNotifications = async (req, res) => {
+    try {
+        const student = await Student.findById(req.session.studentId)
+            .populate({
+                path: 'subscriptions',
+                populate: {
+                    path: 'course',
+                    model: 'Course'
+                }
+            });
+        
+        if (student.hasUnreadNotifications) {
+            student.hasUnreadNotifications = false;
+            await student.save();
+        }
+        
+        res.render('student/student-notifications', { 
+            student,
+            subscriptions: student.subscriptions || []
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server Error');
+    }
+};
+
+exports.subscribe = async (req, res) => {
+    try {
+        const { courseId } = req.body;
+        const studentId = req.session.studentId;
+        
+        const existingSubscription = await Subscription.findOne({
+            student: studentId,
+            course: courseId
+        });
+        
+        if (existingSubscription) {
+            return res.json({ 
+                success: false, 
+                message: 'You are already subscribed to this course' 
+            });
+        }
+        
+        const subscription = new Subscription({
+            student: studentId,
+            course: courseId
+        });
+        
+        await subscription.save();
+        
+        const student = await Student.findById(studentId);
+        student.subscriptions.push(subscription._id);
+        await student.save();
+        
+        res.json({ 
+            success: true, 
+            message: 'Successfully subscribed to course' 
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: 'Server Error' });
+    }
+};
+
+exports.unsubscribe = async (req, res) => {
+    try {
+        const { subscriptionId } = req.body;
+        const studentId = req.session.studentId;
+        
+        const subscription = await Subscription.findById(subscriptionId);
+        
+        if (!subscription) {
+            return res.json({ 
+                success: false, 
+                message: 'Subscription not found' 
+            });
+        }
+        
+        if (subscription.student.toString() !== studentId) {
+            return res.json({ 
+                success: false, 
+                message: 'Unauthorized' 
+            });
+        }
+        
+        const student = await Student.findById(studentId);
+        student.subscriptions = student.subscriptions.filter(
+            sub => sub.toString() !== subscriptionId
+        );
+        await student.save();
+        
+        await Subscription.findByIdAndDelete(subscriptionId);
+        
+        res.json({ 
+            success: true, 
+            message: 'Successfully unsubscribed from course' 
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: 'Server Error' });
+    }
+};
